6b7bcd99eb6dce958dbaaf87ba4e13669326e465
BychkinA <bubblesortt@mail.ru>
2023-03-16 21:10:11 +0300

    4.2, 4.3, 4.4, 4.5, 4.6

--- 4lab/4lab.pl
+++ 4lab/4lab.pl
@@ -6,9 +6,46 @@ size(3, S) :- S >= 170, S < 176.
 size(4, S) :- S >= 176, S < 182.
 size(5, S) :- S >= 182, S < 188.
 
+/*
+Отнимаем размер S от минимального(158) и получаем диапазон 0-29
+Делим на 6(Потому шаг размерной сетки - 6. 158-163,164-169)
+Добавляем 1(обрабатываем случай минимального размера)
+*/
 size(Size, S) :-
     Size is (S - 158) / 6 + 1,
     Size >= 1,
     Size =< 5.
 
 size(Size,159).
+
+% 4.3
+
+%Главное отличие в том, что предикат fail не спрашивает продолжать искать альтернативы или нет, а сразу их ищет
+a:- write(1).
+a:- write(2).
+b(X):- a,X='еще'.
+c:- a.
+d:- a,fail.
+
+% ?-b(X).
+% ?-c.
+% ?-d.
+
+
+% 4.4
+
+country('England','London').
+country('Russia','Moscow').
+country('France','Paris').
+country('China','Pekin').
+country('Japan','Tokyo').
+country('Italy','Rome').
+
+print_capitals :- country(Country, Capital), write(Capital), nl, fail.
+
+% 4.5
+% 121212121212, с предикатом repeat мы будем бесконечно искать альтернативные решения a, а с предикатом fail поиск новых решений будет происходить автоматически
+%repeat,a,fail.
+
+% 4.6
+% repeat, read(X), write(X), X=stop.
